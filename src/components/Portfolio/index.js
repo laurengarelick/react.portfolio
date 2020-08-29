c41ef9563ca21c85fffb737e21cb424cae0ef063
lauren garelick <laurengarelick@gmail.com>
2020-08-29 01:20:09 -0700

    transferring content from stativ port

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,7 @@
+<div class="col-sm-8">
+          <p><Strong>About Me</Strong><br>
+            I am Lauren Garelick. I grew up in southern Orange County and was homeschooled my entire life. 
+            I have always loved being active, exploring, and doing creative projects. So far in life I have 
+            bartended for over ten years and been a personail trainer for six. I am currently enrolled in the UCI full 
+            stack flex developement program to aquire a new set of skills for my next adventure! </p>
+        </div>  
