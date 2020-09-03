a2af446b8ed5cd48c017dcb3174faf5ab081e3ea
lauren garelick <laurengarelick@gmail.com>
2020-09-02 23:46:19 -0700

    new

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,8 @@
-
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { ArrowRight16 } from '@carbon/icons-react';
 
-import { HeaderContainer, Header, Image, ViewResumeLink } from './styles';
+import { HeaderContainer, Header, Image, ViewResumeLink } from './style';
 
 const Header = ({ user }) => {
   const location = useLocation();
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import Navbar from './Navbar'
+export default Navbar
+
+
+
+<h1 class="float-left">
+    <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Lauren Garelick</a>
+      </ul>
+    </h1>
+    <h3 class="float-right">
+      <div style="clear: both"></div>
+        <ul class="nav justify-content-end">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.html">about</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="portfolio.html">portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.html"><strong>contact</strong></a>
+        </li>
+    </ul>
+    </h3>
